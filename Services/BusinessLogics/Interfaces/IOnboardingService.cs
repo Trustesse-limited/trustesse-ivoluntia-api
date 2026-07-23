681e6bfdc968e72b8f525c160b12f437dbf00438
Lemmy731 <lemmy.osiki@decagon.dev>
2026-07-23 17:01:31 +0100

    worked on onboarding

---
+++ Services/BusinessLogics/Interfaces/IOnboardingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trustesse.Ivoluntia.Commons.DTOs;
+using Trustesse.Ivoluntia.Commons.DTOs.Foundation;
+using Trustesse.Ivoluntia.Commons.DTOs.OnboardingDto;
+using Trustesse.Ivoluntia.Commons.Models.Response;
+
+namespace Trustesse.Ivoluntia.Services.BusinessLogics.Interfaces
+{
+    public interface IOnboardingService
+    {
+        Task<GlobalRequestReponse<OnboardingResponseDto>> CreateVolunterOnboarding(VolunteerOnboardingRequestDto volunteerOnboardingDto);
+        Task<GlobalRequestReponse<OnboardingResponseDto>> CreateOrganizationOnboarding(OrganizationOnboardingRequestDto organizationOnboardingDto);
+    }
+}
---
+++ iVoluntia/Controllers/v1/OnboardingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trustesse.Ivoluntia.Commons.DTOs;
+using Trustesse.Ivoluntia.Commons.DTOs.Foundation;
+using Trustesse.Ivoluntia.Commons.DTOs.OnboardingDto;
+using Trustesse.Ivoluntia.Services.BusinessLogics.Interfaces;
+
+namespace Trustesse.Ivoluntia.API.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OnboardingController : BaseController
+    {
+        private readonly IOnboardingService _onboardingService;
+        public OnboardingController(IOnboardingService onboardingService)
+        {
+            _onboardingService = onboardingService;
+        }
+        [HttpPost("volunteer-onboarding")]
+        public async Task<IActionResult> VolunteerOnboarding([FromForm] VolunteerOnboardingRequestDto volunteerOnboardingDto)
+             => BuildHttpResponse<OnboardingResponseDto>(await _onboardingService.CreateVolunterOnboarding(volunteerOnboardingDto.Validate()));
+
+        [HttpPost("organization-onboarding")]
+        public async Task<IActionResult> OrganizationOnboarding([FromForm] OrganizationOnboardingRequestDto organizationOnboardingDto)
+            => BuildHttpResponse<OnboardingResponseDto>(await _onboardingService.CreateOrganizationOnboarding(organizationOnboardingDto.Validate()));
+    }
+}
